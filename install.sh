e27ab5a90877e032669bc9abb6fbd44ab981fe38
tanjiro <tanjiro@mrRobot.mrRobot>
2021-02-19 11:21:29 -0500

    INTILA COMMIT:

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# -----------------CURL-----------------
+echo "Installing CURL"
+sudo apt install curl -y
+
+# -----------------NODE-----------------
+echo "Installing NODE"
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+chmod +x nodesource_setup.sh
+sudo ./nodesource_setup.sh
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt install build-essential -y
+
+# ----------------MONGODB---------------
+echo "Installing MONGODB"
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org -y
+sudo systemctl start mongod.service
+sudo systemctl enable mongod
